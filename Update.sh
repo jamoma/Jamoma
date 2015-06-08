ea0d84d9f1a7cd6d93b01c16aa03ec650e1edf59
Pascal Baltazar <pb@zkrx.org>
2015-06-08 18:34:51 +0200

    updating submodules and adding Update.sh script

---
+++ Update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e -x
+# This script is intended to update all the sub-repos to build a cutting edge i-score with the latest sources available.
+echo "Updating Jamoma umbrella and submodules"
+git pull
+git submodule update
